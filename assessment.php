a341eee0a03d7fc0c544237d3a8fbcc20a0a9482
William Duan <micropresident@gmail.com>
2019-08-03 17:56:53 +1000

    correct data error; judging CLI or browser.

--- assessment.php
+++ assessment.php
@@ -1,12 +1,13 @@
 <?php
 /*
 * DotDev - PHP Developer Test
-* Author:
+* Author: William
 * Date Completed:
 * Time taken: 0h 0m
 * Remarks:
 *   - Modules
-*   - Errors
+*   - Errors Originally 'items' element of $order_items is one-dimensional array rather than multi-dimensional array,
+*     the keys 'id', 'value" and 'name' are duplicate, and so only the last item with the same key is available.
 */
 
 class StoreData {
@@ -31,33 +32,33 @@ class StoreData {
         ];
         $order_items = (object) [
             ['id' => 'DwsNDQ4JDQEEBQIJBAwNBA', 'items' => [
-                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 10.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
-                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
-                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                    ['id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 10.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73'],
+                    ['id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36'],
+                    ['id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772']
                 ]
             ],
             ['id' => 'DwsPBQ0BAA0BBwwMBAoECA', 'items' => [
-                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 90.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
-                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
-                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                    ['id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 90.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73'],
+                    ['id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36'],
+                    ['id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772']
                 ]
             ],
             ['id' => 'DAEFCwUAAgQPAQIIBA4IBA', 'items' => [
-                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 3.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
-                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,  'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
-                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 15.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                    ['id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 3.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73'],
+                    ['id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,  'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36'],
+                    ['id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 15.00, 'name' => 'ecbdb882ae865a07d87611437fda0772']
                 ]
             ],
             ['id' => 'BAUNCAUAAQYMDgULBAMDAQ', 'items' => [
-                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 10.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
-                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
-                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                    ['id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 10.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73'],
+                    ['id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36'],
+                    ['id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772']
                 ]
             ],
             ['id' => 'DAMGAg8GCggLBwkJBAoECg', 'items' => [
-                'id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 32.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73',
-                'id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36',
-                'id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772'
+                    ['id' => 'CgkCDwwDDgYODgYFBAwKAQ', 'value' => 32.00,  'name' => 'b0a8b6f820479900e34d34f6b8a4af73'],
+                    ['id' => 'DQcJBAYFCAoCBAYJBAIGDQ', 'value' => 0.55,   'name' => 'cf3298bb5cbfd41aa44ba18b4f305a36'],
+                    ['id' => 'BwEOBwgNDQ4NCQkHBA8IDA', 'value' => 101.00, 'name' => 'ecbdb882ae865a07d87611437fda0772']
                 ]
             ]
         ];
@@ -72,7 +73,31 @@ class StoreData {
         } elseif ($option = 3) {
             // return orders without items
         }
-        print 'DotDev';
+    }
+}
+
+/**
+ * Is it running in CLI.
+ * @return boolean
+ */
+function is_cli() {
+    return $is_cli = in_array(PHP_SAPI, array('cli', 'cgi', 'cgi-fcgi'));
+}
+
+$running_cli = is_cli();
+$eol = $running_cli ? PHP_EOL : '<br/>';
+
+if (version_compare(phpversion(), '7.0.00', '<')) {
+    echo 'The application needs to run on PHP 7 or higher.' . $eol;
+}
+
+$env = $running_cli ? 'CLI' : 'Browser';
+// determine if the program is running via the CLI or browser
+echo "The script is running from $env" . $eol;
+
+if ($running_cli) {
+    if ($argc !== 2) {
+        echo 'The CLI script needs one and only one option' . $eol;
     }
 }
 
