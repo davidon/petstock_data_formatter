8fb4481ffb3fbb6c2e294b6b87adbce051ea3747
William Duan <micropresident@gmail.com>
2019-08-03 18:45:25 +1000

    optimiazed sort by total

--- assessment.php
+++ assessment.php
@@ -30,12 +30,12 @@ class StoreData {
             ['id' => 'DQkCAAYHAAMJBA4LBAUOCg', 'name' => 'Robot']
         ];
         $orders = (object) [
-            ['id' => 'DwsNDQ4JDQEEBQIJBAwNBA', 'customerId' => 'BQYLCQ0CCwIOBgYNBAcACw', 'dateOrdered' => 1506476504],
-            ['id' => 'DwsPBQ0BAA0BBwwMBAoECA', 'customerId' => 'BQYLCQ0CCwIOBgYNBAcACw', 'dateOrdered' => 1506480104],
-            ['id' => 'DAEFCwUAAgQPAQIIBA4IBA', 'customerId' => 'CQwPDAkDDAQLBQsOBAcMBw', 'dateOrdered' => 1506562904],
-            ['id' => 'BAUNCAUAAQYMDgULBAMDAQ', 'customerId' => 'CgUDCQ8IDAsIBwUBBAgIAA', 'dateOrdered' => 1507081304],
-            ['id' => 'DAMGAg8GCggLBwkJBAoECg', 'customerId' => 'AgsIBAsFAwYCCw8GBAINAQ', 'dateOrdered' => 1509068504],
-            ['id' => 'CQALBwoDAw0AAQgHBAEJBQ', 'customerId' => 'DAEFDQwPDwMCCwULBAAMDg', 'dateOrdered' => 1538012504]
+            ['id' => 'DwsNDQ4JDQEEBQIJBAwNBA', 'customerId' => 'BQYLCQ0CCwIOBgYNBAcACw', 'dateOrdered' => 91506476504],
+            ['id' => 'DwsPBQ0BAA0BBwwMBAoECA', 'customerId' => 'BQYLCQ0CCwIOBgYNBAcACw', 'dateOrdered' => 71506480104],
+            ['id' => 'DAEFCwUAAgQPAQIIBA4IBA', 'customerId' => 'CQwPDAkDDAQLBQsOBAcMBw', 'dateOrdered' => 51506562904],
+            ['id' => 'BAUNCAUAAQYMDgULBAMDAQ', 'customerId' => 'CgUDCQ8IDAsIBwUBBAgIAA', 'dateOrdered' => 81507081304],
+            ['id' => 'DAMGAg8GCggLBwkJBAoECg', 'customerId' => 'AgsIBAsFAwYCCw8GBAINAQ', 'dateOrdered' => 41509068504],
+            ['id' => 'CQALBwoDAw0AAQgHBAEJBQ', 'customerId' => 'DAEFDQwPDwMCCwULBAAMDg', 'dateOrdered' => 61538012504]
         ];
         $order_items = (object) [
             ['id' => 'DwsNDQ4JDQEEBQIJBAwNBA', 'items' => [
@@ -78,30 +78,42 @@ class StoreData {
     public function formatData ($option) {
         // All data should be returned as formatted JSON.
 
-        $orders = $this->formatOrders();
+        $orders = $this->formatOrdersDetails();
         if ($option == 1) {
             // return orders sorted by highest value. Be sure to include the order total in the response
+            $formated = $this->sortOrdersByTotal($orders);
         } elseif ($option == 2) {
             // return orders sorted by date
-            $this->sortOrdersByDate($orders);
+            $formated = $this->sortOrdersByDate($orders);
         } elseif ($option == 3) {
             // return orders without items
         }
+
+        return json_encode($formated);
     }
 
     /**
      * @param $orders
+     * @param $key
      * @return array
      */
-    private function sortOrdersByDate($orders) {
-        foreach($orders as $val) {
-            $dates[] = $val['dateOrdered'];
-            $ordersArr[] = $val;
-        }
-        array_multisort($dates, $ordersArr);
-        return $ordersArr;
+    private function sortOrdersBy($orders, $key) {
+        usort($orders, function ($a, $b) use ($key) {
+            if ($a[$key] == $b[$key]) {
+                return 0;
+            }
+            return ($a[$key] < $b[$key]) ? -1 : 1;
+        });
+        return $orders;
     }
 
+    private function sortOrdersByDate($orders) {
+        return $this->sortOrdersBy($orders,'date');
+    }
+
+    private function sortOrdersByTotal($orders) {
+        return $this->sortOrdersBy($orders,'total');
+    }
 
     /**
      * Get the values of a multi-dimensional array by a sub-key
@@ -109,7 +121,7 @@ class StoreData {
      * @param $attr A multi-dimensional array
      * @return array Values of the sub-key
      */
-    private function arrayValuesRecursive($subkey, array $arr){
+    private function array_values_recursive($subkey, array $arr){
         $val = array();
         array_walk_recursive($arr, function($v, $k) use($subkey, &$val){
             if ($k == $subkey) {
@@ -133,7 +145,7 @@ class StoreData {
     }
 
     private function getItemsTotal(array $items) {
-        $values = $this->arrayValuesRecursive('value', $items);
+        $values = $this->array_values_recursive('value', $items);
         return array_reduce($values, function ($total, $value) {
             $total += $value;
             return $total;
@@ -157,24 +169,28 @@ class StoreData {
         return $orderItemsArray;
     }
 
-    private function formatOrders() {
+    private function formatOrdersDetails() {
         $ordersTotal = $this->getOrdersTotal();
         $customersArray = $this->formatCustomers();
         $orderItemsArray = $this->formatOrderItems();
 
         $newOrders = [];
         foreach ($this->orders as $order) {
+            $customerInfo = isset($customersArray[$order['customerId']]) ? $customersArray[$order['customerId']] : ['id' => $order['customerId']];
+
             $orderId = $order['id'];
             $newOrders[$orderId] = [
                 'date' => $order['dateOrdered'],
-                'total' => $ordersTotal[$orderId],
-                'customer' => $customersArray[$order['customerId']],
+                //if an order has no order items, then total is 0
+                'total' => isset($ordersTotal[$orderId]) ? $ordersTotal[$orderId] : 0,
+                'customer' => $customerInfo,
                 'order_items' => $orderItemsArray[$orderId]
             ];
         }
 
         return $newOrders;
-    }}
+    }
+}
 
 /**
  * Is it running in CLI.
